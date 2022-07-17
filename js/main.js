04d2362e7c70b3400ecfd84e56b32914a915875a
AleksandrDruk <sashamironov25650v@gmail.com>
2022-07-17 22:27:13 +0300

    1
    1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function () {
     $('.burger').click(function () {
-        $('.nav__list').toggleClass('show-menu'),$('.nav__list').toggleClass('animate')
+        $('.nav__list').toggleClass('show-menu'), $('.header').toggleClass('header-bottom')
     })
 });
